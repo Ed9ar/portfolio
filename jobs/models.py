aed6a65e6cf81e30d66494a3aa8f493ced2763b4
ed9ar <a01339939@itesm.mx>
2020-04-05 20:33:47 -0500

    Portfolio
    portfolio

---
+++ jobs/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=200)
+    #url = models.TextField(default='hoihioh')
+
+
+    
